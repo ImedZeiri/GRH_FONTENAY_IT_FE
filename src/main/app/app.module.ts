7ee2dbfcfe3d5b398294247dcb1b2d13b87c6092
ImedZeiri <imedzairi1996@gmail.com>
2023-02-01 11:04:35 +0100

    Initial Achitecture

--- src/main/app/app.module.ts
+++ src/main/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// noinspection AngularInvalidImportedOrDeclaredSymbol
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
